7f02789875291e46b9f3a23ab870066d4cf8d046
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-12-03 02:41:17 +0530

    finished proper logic for login and stuff

--- api/user.ts
+++ api/user.ts
@@ -3,6 +3,9 @@ import { getToken } from "@/lib/handle-session-tokens";
 export const getCurrentUser = async () => {
   try {
     const token = await getToken({ key: "session" });
+    if (!token) {
+      return null;
+    }
     const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/auth/me`, {
       method: "GET",
       headers: {
